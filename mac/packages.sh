5738ca9babe783b448ca296668a81a9d0fa6e8aa
hnegishi <hnegishimaru@gmail.com>
2021-04-10 23:55:42 +0900

    [add] packages

--- mac/packages.sh
+++ mac/packages.sh
@@ -5,6 +5,7 @@
 brew update
 brew upgrade
 brew install peco the_silver_searcher ctags python fzf git fish omf rbenv pyenv direnv wget yarn
-brew cask install visual-studio-code
+# vscode, iterm2, sequel-pro
+brew cask install visual-studio-code iterm2 homebrew/cask-versions/sequel-pro-nightly
 brew install rcm
 
