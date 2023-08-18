7578e4f666cd06e55406578e0ddd4bb15d1908a7
hnegishi <30515665+hnegishi@users.noreply.github.com>
2023-08-18 17:51:06 +0900

    Update packages.sh

--- mac/packages.sh
+++ mac/packages.sh
@@ -6,6 +6,6 @@ brew update
 brew upgrade
 brew install peco the_silver_searcher ctags python fzf git fish omf rbenv pyenv direnv wget yarn
 # vscode, iterm2, sequel-pro
-brew cask install visual-studio-code iterm2 homebrew/cask-versions/sequel-pro-nightly
+brew install --cask visual-studio-code iterm2 homebrew/cask-versions/sequel-pro-nightly tableplus sequel-ace docker
 brew install rcm
 
