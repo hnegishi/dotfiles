6b144d34d44f37974e7353d6f2665e40c0e4e408
hnegishi <30515665+hnegishi@users.noreply.github.com>
2023-08-19 14:49:17 +0900

    Update packages.sh

--- mac/packages.sh
+++ mac/packages.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 brew update
 brew upgrade
-brew install git gitleaks \
-peco the_silver_searcher ctags jq\
+brew install peco the_silver_searcher ctags jq\
 fzf fish omf \
-autoconf 
+autoconf \
 wget yarn \
-rbenv pyenv direnv tfenv python nodenv
-# vscode, iterm2, sequel-pro
+rbenv pyenv nodenv tfenv direnv \
+gitleaks \
+
 brew install --cask visual-studio-code iterm2 tableplus sequel-ace docker
 brew install rcm
 
