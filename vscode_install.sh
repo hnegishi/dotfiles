1765c160a5c4b42d9acb3d7c90beb0c3836f60e9
hnegishi <wxjyg60238@gmail.com>
2023-08-19 15:21:38 +0900

    [modify] vscode_install.sh

--- vscode_install.sh
+++ vscode_install.sh
@@ -3,16 +3,16 @@
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
-rm $VSCODE_SETTING_DIR/settings.json
-ln -s $SCRIPT_DIR/vscode/settings.json ${VSCODE_SETTING_DIR}/settings.json
+rm "$VSCODE_SETTING_DIR/settings.json"
+ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
-# rm $VSCODE_SETTING_DIR/keybindings.json
-# ln -s $SCRIPT_DIR/vscode/keybindings.json ${VSCODE_SETTING_DIR}/keybindings.json
+# rm "$VSCODE_SETTING_DIR/keybindings.json"
+# ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
 # install extention
-cat $VSCODE_SETTING_DIR/vscode/extensions | while read line
+cat "$SCRIPT_DIR/vscode/extensions" | while read line
 do
  code --install-extension $line
 done
 
-code --list-extensions > $VSCODE_SETTING_DIR/vscode/extensions
+code --list-extensions > "$VSCODE_SETTING_DIR/vscode/extensions"
