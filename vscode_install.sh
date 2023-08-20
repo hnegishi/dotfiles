4a946137b3045aba37af6b6d6a7891a975cfb804
hnegishi <wxjyg60238@gmail.com>
2023-08-20 11:32:03 +0900

    [modify] processing into functions

--- vscode_install.sh
+++ vscode_install.sh
@@ -1,18 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
-rm "$VSCODE_SETTING_DIR/settings.json"
-ln -s "$SCRIPT_DIR/vscode/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+symlink_settings() {
+  rm "$VSCODE_SETTING_DIR/settings.json"
+  ln -s "$SCRIPT_DIR/vscode/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+}
 
-# rm "$VSCODE_SETTING_DIR/keybindings.json"
-# ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+# symlink_keybindings() {
+#   rm "$VSCODE_SETTING_DIR/keybindings.json"
+#   ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+# }
 
-# install extention
-cat "$SCRIPT_DIR/vscode/extensions" | while read line
-do
-  code --install-extension $line
-done
+install_extention() {
+  cat "$SCRIPT_DIR/vscode/extensions" | while read line
+  do
+    code --install-extension $line
+  done
+}
 
-code --list-extensions > "$SCRIPT_DIR/vscode/extensions"
+export_vscode_extensions() {
+  code --list-extensions > "$SCRIPT_DIR/vscode/extensions"
+}
+
+main() {
+  symlink_settings
+  # symlink_keybindings
+  install_extention
+  export_vscode_extensions
+}
+
+main "$@"
