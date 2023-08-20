6ad94d3ae82674b9841c7b2eb58ed5bbe41c8230
hnegishi <wxjyg60238@gmail.com>
2023-08-20 11:51:22 +0900

    [remove] commentout

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 set -u
 DOT_DIRECTORY=$(cd $(dirname $0) && pwd)
 # DOT_CONFIG_DIRECTORY=".config"
-# IGNORE_PATTERN="^\.(git|config|chrome|mac|vscode|README.md|install.sh|vscode_install.sh|)"
 
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
@@ -20,24 +19,9 @@ install_ohmyzsh() {
 
 setup_zsh() {
   printf "start setup zsh"
-  # TODO: 今の所ホワイトリスト方式で問題ないが後々ブラックリスト方式へ変更する。
-  # cd ${DOT_DIRECTORY}
-  # for f in .??*; do
-  #     # 無視したいファイルやディレクトリ
-  #     [[ "$f" =~ $IGNORE_PATTERN ]] && continue
-  #     ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
-  # done
   ln -snfv ${DOT_DIRECTORY}/.zshrc ${HOME}/.zshrc
   printf "${GREEN}Success create link.${NC}"
 }
-
-# printf "link .config directory dotfiles"
-# cd ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}
-# for file in `\find . -maxdepth 8 -type f`; do
-# #./の2文字を削除するためにfile:2としている
-#     ln -snfv ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}/${file:2} ${HOME}/${DOT_CONFIG_DIRECTORY}/${file:2}
-# done
-# printf "${GREEN}linked dotfiles complete!${NC}"
 
 main() {
   brew_install
