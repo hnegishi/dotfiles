d113adb748726ede876e800899a3cf58d2322b10
hnegishi <30515665+hnegishi@users.noreply.github.com>
2024-08-02 22:29:03 +0900

    Update packages.sh

--- mac/packages.sh
+++ mac/packages.sh
@@ -6,7 +6,7 @@ brew install the_silver_searcher ctags jq wget gnupg \
 peco zsh-completions zsh-autocomplete zsh-syntax-highlighting zsh-autosuggestions \
 autoconf \
 yarn \
-rbenv pyenv nodenv goenv tfenv direnv \
+asdf rbenv pyenv nodenv goenv tfenv direnv \
 gitleaks
 
 brew install --cask slack visual-studio-code iterm2 warp tableplus sequel-ace docker postman
